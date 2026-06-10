285fa5302a381bbc5056d522bc86158a7aa49263
Johann3421 <1355430@senati.pe>
2026-06-10 11:55:51 -0500

    Subiendo cambios a kenya

--- database/migrations/2026_06_10_000000_normalize_activo_to_uppercase.php
+++ database/migrations/2026_06_10_000000_normalize_activo_to_uppercase.php
@@ -2,31 +2,20 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
     public function up()
     {
-        DB::statement("UPDATE categorias SET activo = 'SI' WHERE UPPER(activo) = 'SI' AND activo != 'SI'");
-        DB::statement("UPDATE categorias SET activo = 'NO' WHERE UPPER(activo) = 'NO' AND activo != 'NO'");
-        DB::statement("UPDATE modelos SET activo = 'SI' WHERE UPPER(activo) = 'SI' AND activo != 'SI'");
-        DB::statement("UPDATE modelos SET activo = 'NO' WHERE UPPER(activo) = 'NO' AND activo != 'NO'");
-        DB::statement("UPDATE marcas SET activo = 'SI' WHERE UPPER(activo) = 'SI' AND activo != 'SI'");
-        DB::statement("UPDATE marcas SET activo = 'NO' WHERE UPPER(activo) = 'NO' AND activo != 'NO'");
-        DB::statement("UPDATE banners SET activo = 'SI' WHERE UPPER(activo) = 'SI' AND activo != 'SI'");
-        DB::statement("UPDATE banners SET activo = 'NO' WHERE UPPER(activo) = 'NO' AND activo != 'NO'");
-        DB::statement("UPDATE soportes SET activo = 'SI' WHERE UPPER(activo) = 'SI' AND activo != 'SI'");
-        DB::statement("UPDATE soportes SET activo = 'NO' WHERE UPPER(activo) = 'NO' AND activo != 'NO'");
-        DB::statement("UPDATE ventas SET activo = 'SI' WHERE UPPER(activo) = 'SI' AND activo != 'SI'");
-        DB::statement("UPDATE ventas SET activo = 'NO' WHERE UPPER(activo) = 'NO' AND activo != 'NO'");
-        DB::statement("UPDATE users SET activo = 'SI' WHERE UPPER(activo) = 'SI' AND activo != 'SI'");
-        DB::statement("UPDATE users SET activo = 'NO' WHERE UPPER(activo) = 'NO' AND activo != 'NO'");
-        DB::statement("UPDATE configuraciones SET activo = 'SI' WHERE UPPER(activo) = 'SI' AND activo != 'SI'");
-        DB::statement("UPDATE configuraciones SET activo = 'NO' WHERE UPPER(activo) = 'NO' AND activo != 'NO'");
-        DB::statement("UPDATE apis SET activo = 'SI' WHERE UPPER(activo) = 'SI' AND activo != 'SI'");
-        DB::statement("UPDATE apis SET activo = 'NO' WHERE UPPER(activo) = 'NO' AND activo != 'NO'");
-        DB::statement("UPDATE manuales SET activo = 'SI' WHERE UPPER(activo) = 'SI' AND activo != 'SI'");
-        DB::statement("UPDATE manuales SET activo = 'NO' WHERE UPPER(activo) = 'NO' AND activo != 'NO'");
+        $tables = ['categorias', 'modelos', 'marcas', 'banners', 'soportes', 'ventas', 'users', 'configuraciones', 'apis', 'manuales'];
+
+        foreach ($tables as $table) {
+            if (Schema::hasTable($table) && Schema::hasColumn($table, 'activo')) {
+                DB::statement("UPDATE {$table} SET activo = 'SI' WHERE UPPER(activo) = 'SI' AND activo != 'SI'");
+                DB::statement("UPDATE {$table} SET activo = 'NO' WHERE UPPER(activo) = 'NO' AND activo != 'NO'");
+            }
+        }
     }
 
     public function down()
