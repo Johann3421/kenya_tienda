c4dc8eff977a5b5fe62dea76ade25d3aca9ec6cb
Johann3421 <1355430@senati.pe>
2026-06-13 10:08:10 -0500

    Elimina Vue mount en #producto_detalle que rompía toggle y layout

--- resources/views/sistema/productos/detalle.blade.php
+++ resources/views/sistema/productos/detalle.blade.php
@@ -600,13 +600,5 @@
 @endsection
 
 @section('js')
-<script>
-    new Vue({
-        el: '#producto_detalle',
-        data: {
-            detalle: null,
-        },
-    });
-</script>
 <script src="https://code.iconify.design/iconify-icon/1.0.0/iconify-icon.min.js"></script>
 @endsection
