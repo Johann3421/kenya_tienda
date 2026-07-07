a752b7282f99d834a27f71b1d5b5fee7d4fad236
Johann3421 <1355430@senati.pe>
2026-07-07 11:50:15 -0500

    fix: use correct App\User namespace

--- app/Http/Controllers/RegistroClienteController.php
+++ app/Http/Controllers/RegistroClienteController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Cliente;
-use App\Models\User;
+use App\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
