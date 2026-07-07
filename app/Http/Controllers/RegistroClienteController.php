ea3d66f7cef8d1e04a3696fa63a5c3dab45027e8
Johann3421 <1355430@senati.pe>
2026-07-07 18:05:46 -0500

    fix: truncate username to 20 characters maximum to match database column limit

--- app/Http/Controllers/RegistroClienteController.php
+++ app/Http/Controllers/RegistroClienteController.php
@@ -62,7 +62,8 @@ class RegistroClienteController extends Controller
                 $user->ape_materno = $request->documento; // Guardamos el documento completo aquí
                 $user->telefono = '000000000';
                 $user->email = $request->correo;
-                $user->username = $request->correo;
+                // username tiene un límite estricto de varchar(20) en la BD, truncamos el correo si es muy largo
+                $user->username = substr($request->correo, 0, 20);
                 $user->password = Hash::make($password);
                 $user->activo = 'SI';
                 $user->save();
