7cee277d35a3b1de57570f9aeac884c78e126863
Johann3421 <1355430@senati.pe>
2026-07-17 15:48:57 -0500

    fix(auth): remove spatie middleware from CotizarController

--- app/Http/Controllers/CotizarController.php
+++ app/Http/Controllers/CotizarController.php
@@ -8,10 +8,7 @@ use Illuminate\Http\Request;
 
 class CotizarController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware(['auth', 'role:cliente_web']);
-    }
+
 
     public function index(Request $request)
     {
